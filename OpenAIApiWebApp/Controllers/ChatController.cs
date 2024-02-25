dc85fc0cc0763dd2a29facb734390040a6031ad0
ufuk murat sınavu <u.murat0606@hotmail.com>
2024-02-25 18:11:25 +0300

    Add project files.

---
+++ OpenAIApiWebApp/Controllers/ChatController.cs
@@ -0,0 +1,41 @@
+﻿using Azure.AI.OpenAI;
+using Azure;
+using Microsoft.AspNetCore.Mvc;
+using OpenAIApiWebApp.Services;
+using System.Threading.Tasks;
+
+namespace OpenAIApiWebApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private readonly ApiService _apiService;
+
+        public ChatController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(string prompt)
+        {
+            if (string.IsNullOrEmpty(prompt) || prompt.Length > 50)
+            {
+                ViewBag.ErrorMessage = "Hata: Mesaj boş olamaz veya en fazla 50 karakter içerebilir.";
+                return View();
+            }
+
+            string responseMessage = await _apiService.SendApiMessage(prompt);
+            ViewBag.ResponseMessage = responseMessage;
+
+            return View();
+        }
+
+        
+    }
+}
---
+++ OpenAIApiWebApp/Services/ApiService.cs
@@ -0,0 +1,39 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+
+namespace OpenAIApiWebApp.Services
+{
+    public class ApiService
+    {
+
+        private readonly string _apiKey;
+        private readonly OpenAIClient _openAIClient;
+
+        public ApiService(string apiKey)
+        {
+            _apiKey = apiKey;
+            _openAIClient = new OpenAIClient(apiKey);
+        }
+
+        public async Task<string> SendApiMessage(string prompt)
+        {           
+
+            var chatCompletionsOptions = new ChatCompletionsOptions()
+            {
+                DeploymentName = "gpt-3.5-turbo-0125",
+                Messages =
+                      {
+                        new ChatRequestSystemMessage("Sen türk dil kurumunda çalışan bir asistansın ve görevin cümledeki yanlış kelimeleri düzeltmek."),
+
+                        new ChatRequestUserMessage(prompt),
+
+                      }
+            };
+
+            Response<ChatCompletions> response = await _openAIClient.GetChatCompletionsAsync(chatCompletionsOptions);
+            ChatResponseMessage responseMessage = response.Value.Choices[0].Message;
+            return $"[{responseMessage.Role.ToString().ToUpperInvariant()}]: {responseMessage.Content}";
+        }
+    }
+    
+}
